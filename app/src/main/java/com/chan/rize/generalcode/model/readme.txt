9e7a50ff0a8389f786426a3225e27cb28cbdadf7
chan <bywyc@live.com>
2018-01-31 21:53:39 +0800

    补充结构及说明

---
+++ app/src/main/java/com/chan/rize/generalcode/model/readme.txt
@@ -0,0 +1,7 @@
+model包存放数据存储，这个包里需要做单例，并考虑同步
+包括：
+1.sharedpreference数据处理
+2.database数据处理
+3.web数据请求
+4.file数据处理
+or other数据
--- app/src/main/java/com/chan/rize/generalcode/view/MainActivity.java
+++ app/src/main/java/com/chan/rize/generalcode/view/MainActivity.java
@@ -1,7 +1,9 @@
-package com.chan.rize.generalcode;
+package com.chan.rize.generalcode.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.chan.rize.generalcode.R;
 
 public class MainActivity extends AppCompatActivity {
 
